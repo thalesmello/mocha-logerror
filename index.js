cfa69e30138b83550e997b430f8795b146c131e9
Thales Mello <thalesmello@gmail.com>
2016-10-31 21:11:54 -0200

    Opt to override message with more descriptive error

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const { Runner } = require('mocha')
 
 const old = Runner.prototype.fail;
 Runner.prototype.fail = function (test, err) {
-  console.log(JSON.stringify(err, null, 4))
+  if (err instanceof Error) {
+    let extraProperties = JSON.parse(JSON.stringify(err))
+    let extraWithMessage = Object.assign(extraProperties, { message: err.message })
+    err.message = JSON.stringify(extraWithMessage, null, 4)
+  }
+
   return old.call(this, test, err);
 };
